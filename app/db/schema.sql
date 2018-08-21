46ff00414f6b8614a3197d03087c87135f2fd93f
student_32 <student_32@ool-ad02060a.dyn.optonline.net>
2018-08-21 11:05:31 -0400

    switched timestamp to comments

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS assignments (
   id SERIAL PRIMARY KEY,
   date DATE,
   github_link VARCHAR(400),
-  created_at TIMESTAMP,
   type VARCHAR(50),
   unit VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS comments (
   id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP,
   assignment_id integer,
   comment text
 );
