51a94b356fdae53f06dd0d5b0d3557d4e8554dcb
Tom DeVito <tomdevito27@gmail.com>
2018-08-21 10:24:14 -0400

    assignment form

--- client/src/Assignments.jsx
+++ client/src/Assignments.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 
 export default function Assignments(props) {
     return (
-        props.assignments.map(assignment => {
-            return (
-                <div key={assignment.id}>
+       props.assignments.map(assignment => {
+           return (
+               <div key={assignment.id}>
                     
-                    <h1>{assignment.title}</h1>
-                    <button>Click</button>
+                   <h1>{assignment.title}</h1>
+                   <button>Click</button>
                     
                     <form action="" method="post">
-                    <input type="text" name="" id=""/>
-                    <input type="button" value=""/>
+                        <input type="text" value="Title" name="title" id=""/>
+                            <br/>
+                            <br/>
+                        <input type="date" name="date" id=""/>
+                            <br/>
+                            <br/>
+                        <input type="text" value="GitHu Link" name="github-link" id=""/>
+                            <br/>
+                            <br/>
+                        <input type="text" value="Type" name="type" id=""/>
+                            <br/>
+                            <br/>
+                        <input type="text" value="Unit" name="unit" id=""/>
+                            <br/>
+                            <br/>
+                        <input type="button" value="Add a new assignment"/>
                     </form>
                 
-                </div>
-            )
-        })
+               </div>
+           )
+       })
     );
 }
