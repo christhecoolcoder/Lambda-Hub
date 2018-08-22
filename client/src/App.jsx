20b7a9bbd764b51a20dd04cea34227a363982f05
rob <robertsalguerojr@gmail.com>
2018-08-22 13:17:43 -0400

    completed createNewAssignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Assignments from './Assignments';
-import { fetchAssignments, fetchAssignment, deleteAssignment } from './services/api';
-import AssignmentForm from './AssignmentForm';
+import { fetchAssignments, fetchAssignment, deleteAssignment, saveAssignment } from './services/api';
+import CreateAssignment from './CreateAssignment';
 import AssignmentDetail from './AssignmentDetail';
 
 export default class App extends Component {
@@ -16,6 +16,7 @@ export default class App extends Component {
     }
     this.handleDelete = this.handleDelete.bind(this);
     this.fetchOneAssignment = this.fetchOneAssignment.bind(this);
+    this.newAssignment = this.newAssignment.bind(this);
   }
 
   fetchAllAssignments() {
@@ -29,46 +30,62 @@ export default class App extends Component {
     this.fetchAllAssignments();
   }
 
-  determineWhichToRender() {
-    const { currentView } = this.state;
-    const { assignments, assignment } = this.state;
-
-    switch (currentView) {
-      case 'Detail':
-        return <AssignmentDetail assignment={assignment} />
-        break;
-      case 'Assignments':
-        return  <Assignments assignments={assignments} handleDelete={this.handleDelete} fetchOneAssignment={this.fetchOneAssignment}/>
-        break;
-    }
-  }
-
+  
   fetchOneAssignment(id) {
     fetchAssignment(id)
-      .then(assignment => {
-        this.setState({  
-          assignment,
-          currentView: 'Detail', });
+    .then(assignment => {
+      this.setState({  
+        assignment,
+        currentView: 'Detail', });
       });
-  }
-
-  handleDelete(id) {
-    deleteAssignment(id)
+    }
+    
+    handleDelete(id) {
+      deleteAssignment(id)
       .then(res => {
         this.fetchAllAssignments();
       })
-  }
+    }
 
-  render() {
-
-    const links = [
-      'Assignments',
-      'Detail'
-    ];
+    newAssignment(assignment) {
+      saveAssignment(assignment)
+        .then(({assignments}) => fetchAssignments())
+        .then(({assignments}) => {
+          this.setState({
+            currentView: 'Assignments',
+            assignments:  assignments ,
+          });
+        });
+    }
+    determineWhichToRender() {
+      const { currentView } = this.state;
+      const { assignments, assignment } = this.state;
+  
+      switch (currentView) {
+        case 'Detail':
+          return <AssignmentDetail assignment={assignment} />
+          break;
+        case 'Assignments':
+          return  <Assignments assignments={assignments} handleDelete={this.handleDelete} fetchOneAssignment={this.fetchOneAssignment}/>
+          break;
+        case 'Create Assignment':
+          return <CreateAssignment 
+            onSubmit={this.newAssignment} />;
+          break;
+      }
+    }
+    
+    render() {
+      
+      const links = [
+        'Assignments',
+        'Detail',
+        'Create Assignment'
+      ];
     return (
       <div className="App">
         <h1>Hello LambdaHub!</h1>
-        <AssignmentForm />
+        <CreateAssignment  onSubmit={this.newAssignment} />
         <h1>Lambda Hub</h1>
         {this.determineWhichToRender()}
        
