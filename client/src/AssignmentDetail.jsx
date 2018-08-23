b5605b5a8278b7ca80abdcf0ce75880186ec1692
student_32 <student_32@ool-ad02060a.dyn.optonline.net>
2018-08-23 15:01:09 -0400

    added moment package and changed assignmentdetail html

--- client/src/AssignmentDetail.jsx
+++ client/src/AssignmentDetail.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function AssignmentDetail(props) {
     
     return (
         <div>
             <h1>{props.assignment.name}</h1>
-            <h1>Created at{props.assignment.date}</h1>
-            <h1>{props.assignment.github_link}</h1>
+            <h1>Created on {moment(props.assignment.date).format('MMM Do')}</h1>
+            <a href={props.assignment.github_link}><h1>{props.assignment.github_link}</h1></a>
             <h1>{props.assignment.type}</h1>
-            <h1>{props.assignment.unit}</h1>
+            <h1>unit {props.assignment.unit}</h1>
             <h1>{props.assignment.content}</h1>
 
        </div>
