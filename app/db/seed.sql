a41c5e16c49ff7c357686a8b13b6aa080cb47d2a
student_32 <student_32@ool-ad02060a.dyn.optonline.net>
2018-09-12 16:46:39 -0400

    fixed schema/seed for deploment

--- app/db/seed.sql
+++ app/db/seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO assignments (name, date, github_link, type, unit) VALUES 
-  ('Hello World!', '03/16/1990', 'link1', 'type1', 'unit1'),
-  ('Hello World!', '03/16/1990', 'link2', 'type2', 'unit2'),
-  ('Hello World!', '03/16/1990', 'link3', 'type1', 'unit1'),
-  ('Hello World!', '03/16/1990', 'link1', 'type1', 'unit4')
+  ('Hello World!', '03/16/1990', 'link1', 'type1', '1'),
+  ('Hello World!', '03/16/1990', 'link2', 'type2', '2'),
+  ('Hello World!', '03/16/1990', 'link3', 'type1', '1'),
+  ('Hello World!', '03/16/1990', 'link1', 'type1', '4')
