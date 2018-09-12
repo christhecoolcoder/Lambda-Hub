d8ec2668833f647c67245f74b2f3242f88e7437d
student_32 <student_32@ool-ad02060a.dyn.optonline.net>
2018-09-12 17:23:37 -0400

    fixed deployment 100% crud working

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://quiet-refuge-38665.herokuapp.com';
 
 export function fetchAssignments() {
     return fetch(BASE_URL + '/assignments')
